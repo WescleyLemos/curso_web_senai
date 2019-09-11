9149e69e0c4f77fdfeb3f57203bd5e8975668535
Wescley Ribeiro <wescley188@gmail.com>
2019-09-11 16:31:21 -0300

    Atualização dos estudos de ajax - spa

--- AJAX/ajax/server.js
+++ AJAX/ajax/server.js
@@ -12,4 +12,28 @@ app.use(bodyParser.json()); //usado para transformar json em informa~ções trat
 
 app.get('/teste', (req, res) => res.send('Ok!'));
 
+
+
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocorreu um erro no envio do arquivo')
+        } else { res.end('Concluído com sucesso"') }
+    })
+})
+
 app.listen(8080, () => console.log('executando...'));
